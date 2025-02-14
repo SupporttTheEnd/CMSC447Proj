c183df04560344ef9c8d17ed679405991ef89509
Andrew Tang <andrewtang0708@gmail.com>
2025-02-13 20:58:46 -0500

    Initial commit

--- controller.js
+++ controller.js
@@ -230,7 +230,7 @@ function toggleSidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.classList.toggle('hide');
     if (sidebar.classList.contains('hide')) {
-        sidebar.style.top = '20px';
+        sidebar.style.top = '20%';
         sidebar.style.left = '0';
     }
 }
