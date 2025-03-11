e295312bfa3b826c2e551a206f8f0103d3e54ec8
SupporttTheEnd <andrewtang0708@gmail.com>
2025-03-11 17:00:50 -0400

    Fixed cross check for minor and major requirements

--- codebase/home.js
+++ codebase/home.js
@@ -20,18 +20,26 @@ function initializeSelect2() {
 
 function setupMajorMinorValidation() {
     $('#major').on('change', function () {
-        let selectedMajors = $(this).val() || [];
+        let selectedMajors = $(this).find(':selected').map(function () {
+            return $(this).text();
+        }).get();
+
         $('#minor option').each(function () {
-            $(this).prop('disabled', selectedMajors.includes($(this).val()));
+            $(this).prop('disabled', selectedMajors.includes($(this).text()));
         });
+
         $('#minor').trigger('change.select2');
     });
 
     $('#minor').on('change', function () {
-        let selectedMinors = $(this).val() || [];
+        let selectedMinors = $(this).find(':selected').map(function () {
+            return $(this).text();
+        }).get();
+
         $('#major option').each(function () {
-            $(this).prop('disabled', selectedMinors.includes($(this).val()));
+            $(this).prop('disabled', selectedMinors.includes($(this).text()));
         });
+
         $('#major').trigger('change.select2');
     });
 }
@@ -182,7 +190,7 @@ async function populateClass(className) {
     `;
 
     const result = db.exec(query);
-    if (!result.length) return;  
+    if (!result.length) return;
 
     const combinedData = result[0].values.map(row => {
         return {
@@ -221,7 +229,7 @@ async function populateClass(className) {
 
 function clearClasses() {
     const dropzones = document.querySelectorAll(`#classes .dropzone`);
-    
+
     dropzones.forEach(dropzone => {
         dropzone.innerHTML = "";
     });
