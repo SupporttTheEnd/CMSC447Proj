3ecde419f0f42ea6020e4aff71b387a925730ca1
SupporttTheEnd <andrewtang0708@gmail.com>
2025-04-15 16:07:58 -0400

    fixed some styling as well as added just the buttons for the save and load slots

---
+++ codebase/saves.js
@@ -0,0 +1,19 @@
+import { createMessage } from './login.js';
+
+export async function main() {
+    await getAccountData();
+    document.getElementById("save-button").addEventListener("click", postNewNote);
+    document.getElementById("save-button").addEventListener("click", postNewNote);
+}
+
+async function getSaveData () {
+    
+}
+
+async function enterSaveData () {
+
+}
+
+async function loadSaveData () {
+    
+}
