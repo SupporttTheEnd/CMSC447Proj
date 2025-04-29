4ef29d455c161c158eb2d0bfe08d338a50dea305
Bryn <p.a.baillif@gmail.com>
2025-04-29 17:08:18 -0400

    recoded on andrew's request

--- codebase/balance.js
+++ codebase/balance.js
@@ -1,83 +1,37 @@
 import { generateYears } from './home.js';
 
 export function balanceClassData() {
-    let classesArray = [], requirementsArray = [];
+    let classesArray = [];
 
-    let j = 0;
-    for (let i = 0; i <= window.globalVariables.years; i++) {
+    for (let i = 1; i <= window.globalVariables.years; i++) {
         const yearContainer = document.querySelector(`.container.year-${i}`);
-        if (!yearContainer) continue;
 
-        Array.from(yearContainer.querySelectorAll(".dropzone")).forEach(dropzone => {
-            classesArray[j] = [];
-            requirementsArray[j] = [];
-            Array.from(dropzone.querySelectorAll(".class-item")).forEach(classItem => {
-                if (classItem.classList.contains("require-item")) {
-                    requirementsArray[j].push(classItem);
-                } else {
-                    classesArray[j].push(classItem);
-                }
+        yearContainer.querySelectorAll(".dropzone").forEach(dropzone => {
+            dropzone.querySelectorAll(".class-item").forEach(classItem => {
+                classesArray.push(classItem);
+                classItem.remove();
             });
-            j++
         });
     }
 
-    let currYear = 1;
-    let tempYear = 0;
-    let currSem = 0;
-    let currClassesInSem = 0;
-    const maxClassesInSem = 5;
-    let yearContainer = document.querySelector(`.container.year-${1}`);
-    for (let i = 1; i <= 8; i++) {
-        let classIndex = 0;
-        let requirementIndex = 0;
+    for (let i = 1; i <= window.globalVariables.years; i++) {
+        const yearContainer = document.querySelector(`.container.year-${i}`);
 
-        
-        while (classIndex < classesArray[i].length || requirementIndex < requirementsArray[i].length) {
-            
-            if (classIndex < classesArray[i].length) {
-                Array.from(yearContainer.querySelectorAll(".dropzone"))[currSem].appendChild(classesArray[i][classIndex]);
-                classIndex++;
-                currClassesInSem++;
-
-                [tempYear, currSem, currClassesInSem] = checkBounds(currYear, currSem, currClassesInSem, maxClassesInSem);
-                if (tempYear != currYear) {
-                    yearContainer = document.querySelector(`.container.year-${tempYear}`);
-                    currYear = tempYear;
-                }
-            }
-
-            if (requirementIndex <  requirementsArray[i].length) {
-                Array.from(yearContainer.querySelectorAll(".dropzone"))[currSem].appendChild(requirementsArray[i][requirementIndex])
-                requirementIndex++;
-                currClassesInSem++;
-
-                [tempYear, currSem, currClassesInSem] = checkBounds(currYear, currSem, currClassesInSem, maxClassesInSem);
-                if (tempYear != currYear) {
-                    yearContainer = document.querySelector(`.container.year-${tempYear}`);
-                    currYear = tempYear;
-                }
+        for (let dropzone of yearContainer.querySelectorAll(".dropzone")) {    
+            for (let currClasses = 0; currClasses < 5 && classesArray.length > 0; currClasses++) {
+                dropzone.appendChild(classesArray.shift());
             }
         }
-    }
-}
-
-function checkBounds(currYear, currSem, currClassesInSem, maxClassesInSem){
-    if (currClassesInSem >= maxClassesInSem) {
-        if (currYear == window.globalVariables.years && currSem == 1) {
+    
+        if (classesArray.length > 0 && i === window.globalVariables.years) {
             generateYears(true);
         }
-        
-        if (currSem == 1) {
-            currYear++;
-            currSem = 0;
-        }
-        else {
-            currSem = 1;
-        }
-
-        currClassesInSem = 0;
     }
-    
-    return [currYear, currSem, currClassesInSem];
+
+    // Additional functionality to move require items to the bottom of the dropzones
+    document.querySelectorAll("#classes .dropzone").forEach(dropzone => {
+        dropzone.querySelectorAll(".require-item").forEach(classItem => {
+            dropzone.appendChild(classItem);
+        });
+    });
 }
